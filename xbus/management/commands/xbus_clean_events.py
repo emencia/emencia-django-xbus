ca9645ebcac3cacfe3b5942ad8862610276c7aaa
Lafaye Philippe <lafaye@emencia.com>
2016-09-23 12:17:40 +0200

    add a script that delete old xbus_events

---
+++ xbus/management/commands/xbus_clean_events.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ValidationError
+from xbus.models import Event
+
+class Command(BaseCommand):
+    help = "Delete xbus event from db older than a specific period or date"
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument(
+            'period',
+            nargs='?', type=int, default=0,
+            help="Set a number of days")
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--date',
+            nargs='?', dest='date', type=str, default='',
+            help='Use a date in the format yyyy-mm-dd rather than a period')
+        parser.add_argument(
+            '--noinput', '--no-input',
+            action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.',
+        )
+
+    def handle(self, *args, **options):
+        date = options['date']
+        days = options['period']
+        interactive = options['interactive']
+
+        if days:
+            ctime = datetime.now() - timedelta(days)
+        else:
+            ctime = date
+        try:
+            query = Event.objects.filter(ctime__lte=ctime)
+            nb = query.count()
+        except ValidationError:
+            print "Wrong argument format, read --help for more help"
+        else:
+            helper = "The {} xbus events older than {} will be delete"
+            print helper.format(nb, ctime)
+            if interactive:
+                answer = raw_input('Are you sure ? (yes or no): ')
+            else:
+                answer = 'yes'
+            if answer.lower() in ['yes', 'y']:
+                query.delete()
+                print "Deleted"
+            else:
+                print "Aborted"
